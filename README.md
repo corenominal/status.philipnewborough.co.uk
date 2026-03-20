0eec7848851104354906b8c695af724e6c241319
Philip Newborough <corenominal@corenominal.org>
2026-03-20 09:57:07 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# status.philipnewborough.co.uk
---
+++ app/Config/Urls.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class Urls extends BaseConfig
+{
+    public $tld = '';
+    public $startpage = '';
+    public $appmenu = '';
+    public $auth = '';
+    public $assets = '';
+    public $sendmail = '';
+    public $notifications = '';
+    public $metrics = '';
+    public $logs = '';
+    public $markdown = '';
+    public $license = '';
+    public $github = '';
+    public $tldcookiedomain = '';
+    public $cookiedomain = '';
+}
