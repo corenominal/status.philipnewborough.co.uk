c40c0581b57f42096b03da2b3d5555b24ab914a7
Philip Newborough <corenominal@corenominal.org>
2026-03-20 10:20:20 +0000

    Add Mastodon configuration class

---
+++ app/Config/Mastodon.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class Mastodon extends BaseConfig
+{
+    public $url          = '';
+    public $apiv1        = '';
+    public $apiv2        = '';
+    public $access_token = '';
+    public $account      = '';
+    public $profile      = '';
+}
